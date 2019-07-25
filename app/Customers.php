a9ac99de07087952e74ac93d7ba7ab78c886d418
fireb1001 <fireb1001@gmail.com>
2019-07-25 23:12:53 +0200

    customers

--- app/Customers.php
+++ app/Customers.php
@@ -3,9 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Customers extends Model
 {
+    
     use SoftDeletes;
-    //
+
+    protected $fillable = [
+        'name', 'debt', 'phone','address','notes','is_self'
+    ];
 }
